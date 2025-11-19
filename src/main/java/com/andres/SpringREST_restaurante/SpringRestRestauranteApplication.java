e8a55b9db6736556a336b8e8b977b8496b5f8cf5
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-19 13:34:07 +0100

    initial commit

---
+++ src/main/java/com/andres/SpringREST_restaurante/SpringRestRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.andres.SpringREST_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andres/SpringREST_restaurante/SpringRestRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.SpringREST_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
