40b84eb1679b06acc8aa9756791aa7077b9470c9
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-22 13:26:48 +0100

    fixed import

--- src/main/java/com/andres/SpringREST_restaurante/repositories/ProviderRepository.java
+++ src/main/java/com/andres/SpringREST_restaurante/repositories/ProviderRepository.java
@@ -1,9 +1,9 @@
 package com.andres.SpringREST_restaurante.repositories;
 
-import java.security.Provider;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.andres.SpringREST_restaurante.entities.Provider;
 
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
