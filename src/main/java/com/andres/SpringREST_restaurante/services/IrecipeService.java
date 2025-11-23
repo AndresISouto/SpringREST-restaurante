3e3e44a3f792d95f85b008165da151d8e2af1b03
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-23 11:35:09 +0100

    define methods

--- src/main/java/com/andres/SpringREST_restaurante/services/IrecipeService.java
+++ src/main/java/com/andres/SpringREST_restaurante/services/IrecipeService.java
@@ -1,9 +1,12 @@
 package com.andres.SpringREST_restaurante.services;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.andres.SpringREST_restaurante.entities.Recipe_Ingredient;
+import com.andres.SpringREST_restaurante.entities.DTO.IngredientResponseDTO;
 import com.andres.SpringREST_restaurante.entities.DTO.RecipeRequestDTO;
 import com.andres.SpringREST_restaurante.entities.DTO.RecipeResponseDTO;
 import com.andres.SpringREST_restaurante.entities.DTO.RecipeUpdateDTO;
@@ -23,4 +26,15 @@ public interface IrecipeService {
   RecipeResponseDTO update(RecipeUpdateDTO dto);
 
   List<ReviewDTO> getReviewsById(Long id);
+
+  List<IngredientResponseDTO> getIngredientsById(Long id);
+
+  Double calculatePrice(Long id);
+
+  Double calculatePrice(Set<Recipe_Ingredient> ri);
+
+  void setPrice(Long id);
+
+  void setPriceWithIngredient(Long id);
+
 }
