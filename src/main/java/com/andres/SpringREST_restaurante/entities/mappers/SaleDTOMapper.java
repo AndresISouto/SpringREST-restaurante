0662a0cf2b17f789a5a52d06f119d0f620a6b76a
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-24 09:29:13 +0100

    fixed mapper

--- src/main/java/com/andres/SpringREST_restaurante/entities/mappers/SaleDTOMapper.java
+++ src/main/java/com/andres/SpringREST_restaurante/entities/mappers/SaleDTOMapper.java
@@ -1,21 +1,25 @@
 package com.andres.SpringREST_restaurante.entities.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.andres.SpringREST_restaurante.entities.Recipe;
 import com.andres.SpringREST_restaurante.entities.Sale;
 import com.andres.SpringREST_restaurante.entities.Sale_Recipe;
 import com.andres.SpringREST_restaurante.entities.DTO.RecipeSaleDTO;
-import com.andres.SpringREST_restaurante.entities.DTO.SaleDTO;
+import com.andres.SpringREST_restaurante.entities.DTO.SaleResponseDTO;
 
 @Mapper(componentModel = "spring")
 public interface SaleDTOMapper {
 
   @Mapping(target = "recipes", source = "recipes")
-  SaleDTO toDto(Sale sale);
+  SaleResponseDTO toDto(Sale sale);
 
-  @Mapping(target = "name", source = "recipe.name")
+  @Mapping(target = "recipes", source = "recipes")
+  List<SaleResponseDTO> toDto(List<Sale> sales);
+
+  @Mapping(target = "recipe_id", source = "recipe.recipe_id")
   @Mapping(target = "amount", source = "amount")
   RecipeSaleDTO toRecipeSaleDto(Sale_Recipe sale_recipe);
 
