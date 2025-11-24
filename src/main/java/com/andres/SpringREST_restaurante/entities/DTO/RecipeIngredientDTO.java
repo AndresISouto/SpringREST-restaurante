9b66687407d929d0f1d097e550406d662172bbc0
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-24 12:30:28 +0100

    validation

--- src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeIngredientDTO.java
+++ src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeIngredientDTO.java
@@ -2,8 +2,11 @@ package com.andres.SpringREST_restaurante.entities.DTO;
 
 import com.andres.SpringREST_restaurante.Enums.Unit;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record RecipeIngredientDTO(
-    Long ingredient_id,
-    Unit unit,
-    Double amount) {
+    @NotNull @Positive Long ingredient_id,
+    @NotNull Unit unit,
+    @NotNull @Positive Double amount) {
 }
--- src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeRequestDTO.java
+++ src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeRequestDTO.java
@@ -2,10 +2,17 @@ package com.andres.SpringREST_restaurante.entities.DTO;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record RecipeRequestDTO(
-    String name,
-    Integer portions,
-    String steps,
-    Double precioVenta,
-    List<RecipeIngredientDTO> ingredients) {
+
+    @NotBlank String name,
+    @NotNull @Positive Integer portions,
+    @NotBlank String steps,
+    @NotNull @Positive Double precioVenta,
+    @NotEmpty @Valid List<RecipeIngredientDTO> ingredients) {
 }
