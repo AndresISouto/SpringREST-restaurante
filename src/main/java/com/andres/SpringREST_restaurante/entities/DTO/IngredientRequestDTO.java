a07c8354d55ed8a390bcd2b600b801f6f19dbc7a
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-26 11:37:44 +0100

    add validation

--- src/main/java/com/andres/SpringREST_restaurante/entities/DTO/IngredientRequestDTO.java
+++ src/main/java/com/andres/SpringREST_restaurante/entities/DTO/IngredientRequestDTO.java
@@ -2,9 +2,13 @@ package com.andres.SpringREST_restaurante.entities.DTO;
 
 import com.andres.SpringREST_restaurante.Enums.Unit;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record IngredientRequestDTO(
-    String name,
-    Double amount,
-    Unit unit,
-    Double pricePerUnit) {
+    @NotNull @NotBlank String name,
+    @NotNull @Positive Double amount,
+    @NotNull Unit unit,
+    @NotNull @Positive Double pricePerUnit) {
 }
