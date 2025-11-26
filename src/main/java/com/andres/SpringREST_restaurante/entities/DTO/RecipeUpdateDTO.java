7210e75e906f53bcdd523e4266f337578a2bc736
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-26 11:37:25 +0100

    add validation

--- src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeUpdateDTO.java
+++ src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeUpdateDTO.java
@@ -2,11 +2,16 @@ package com.andres.SpringREST_restaurante.entities.DTO;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record RecipeUpdateDTO(
-    Long recipe_id,
-    String name,
-    Integer portions,
-    String steps,
-    Double precioVenta,
-    List<RecipeIngredientDTO> ingredients) {
+    @NotNull @Positive Long recipe_id,
+    @NotBlank String name,
+    @NotNull @Positive Integer portions,
+    @NotBlank String steps,
+    @NotEmpty @Valid List<RecipeIngredientDTO> ingredients) {
 }
