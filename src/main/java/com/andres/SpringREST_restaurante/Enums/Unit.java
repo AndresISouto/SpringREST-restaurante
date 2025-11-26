ce586b0275e770c81929b97f7933d794ccd3fcd2
AndresISouto <AndresISouto@users.noreply.github.com>
2025-11-26 13:34:43 +0100

    add unit conversion logic

--- src/main/java/com/andres/SpringREST_restaurante/Enums/Unit.java
+++ src/main/java/com/andres/SpringREST_restaurante/Enums/Unit.java
@@ -1,5 +1,25 @@
 package com.andres.SpringREST_restaurante.Enums;
 
 public enum Unit {
-  Kg, L
+  Kg(UnitCategory.MASS, 1.0),
+  G(UnitCategory.MASS, 0.001),
+
+  L(UnitCategory.VOLUME, 1.0),
+  ML(UnitCategory.MASS, 0.001);
+
+  private final UnitCategory category;
+  private final Double conversionFactor;
+
+  private Unit(UnitCategory category, Double caonversionFactor) {
+    this.category = category;
+    this.conversionFactor = caonversionFactor;
+  }
+
+  public static double convert(Unit unit, double value) {
+    if (value < 0) {
+      throw new IllegalArgumentException("Value cannot be negative");
+    }
+    return (value * unit.conversionFactor);
+  }
+
 }
