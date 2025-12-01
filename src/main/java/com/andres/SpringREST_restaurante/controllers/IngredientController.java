b73910ad06476712666b9e08cb48200332d5b6bc
AndresISouto <AndresISouto@users.noreply.github.com>
2025-12-01 12:16:57 +0100

    fixed http method

--- src/main/java/com/andres/SpringREST_restaurante/controllers/IngredientController.java
+++ src/main/java/com/andres/SpringREST_restaurante/controllers/IngredientController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +56,7 @@ public class IngredientController {
     return ResponseEntity.ok(response);
   }
 
-  @PostMapping("/v1/ingredients/{id}")
+  @PutMapping("/v1/ingredients/{id}")
   public ResponseEntity<IngredientResponseDTO> update(@RequestBody @Valid IngredientUpdateDTO dto) {
     IngredientResponseDTO response = ingredientService.update(dto);
     return ResponseEntity.ok(response);
