3bece9736b7ea625e1c36f46ca7137572e1eae15
AndresISouto <AndresISouto@users.noreply.github.com>
2025-12-01 10:42:00 +0100

    fixed endpoint method

--- src/main/java/com/andres/SpringREST_restaurante/controllers/RecipeController.java
+++ src/main/java/com/andres/SpringREST_restaurante/controllers/RecipeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,7 +57,7 @@ public class RecipeController {
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 
-  @PostMapping("/v1/recipes/{id}")
+  @PutMapping("/v1/recipes/{id}")
   public ResponseEntity<RecipeResponseDTO> update(@RequestBody @Valid RecipeUpdateDTO dto) {
     RecipeResponseDTO response = recipeService.update(dto);
     return ResponseEntity.ok(response);
