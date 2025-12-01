c77cb6e7a1cfb2729c8d63a6df1fda686b72caff
AndresISouto <AndresISouto@users.noreply.github.com>
2025-12-01 12:31:05 +0100

    fixed validation error

--- src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeSaleDTO.java
+++ src/main/java/com/andres/SpringREST_restaurante/entities/DTO/RecipeSaleDTO.java
@@ -1,9 +1,9 @@
 package com.andres.SpringREST_restaurante.entities.DTO;
 
-import io.micrometer.common.lang.NonNull;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record RecipeSaleDTO(
-    @NonNull @Positive Long recipe_id,
-    @NonNull @Positive Integer amount) {
+    @NotNull @Positive Long recipe_id,
+    @NotNull @Positive Integer amount) {
 }
