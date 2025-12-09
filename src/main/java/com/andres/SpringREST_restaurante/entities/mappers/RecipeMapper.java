7777cee81cd07ad82dc8adff4b8b7bca612bf84a
AndresISouto <AndresISouto@users.noreply.github.com>
2025-12-09 11:47:27 +0100

    remove unnecesary comments

--- src/main/java/com/andres/SpringREST_restaurante/entities/mappers/RecipeMapper.java
+++ src/main/java/com/andres/SpringREST_restaurante/entities/mappers/RecipeMapper.java
@@ -13,18 +13,14 @@ import com.andres.SpringREST_restaurante.entities.DTO.RecipeUpdateDTO;
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
 
-  // 1. Main mapping: Recipe -> RecipeResponseDTO
-  // MapStruct automatically detects the Set -> List conversion for 'ingredients'
   @Mapping(target = "ingredients", source = "ingredients")
   RecipeResponseDTO toDto(Recipe recipe);
 
-  // 2. Helper mapping: Recipe_Ingredient -> IngredientResponseDTO
-  // This runs automatically for every item in the Set
-  @Mapping(target = "name", source = "ingredient.name") // Jump from Link -> Ingredient -> Name
-  @Mapping(target = "amount", source = "amount") // Directly from Link
-  @Mapping(target = "unit", source = "unit") // Directly from Link (Enums map to String auto)
-  @Mapping(target = "ingredient_id", source = "ingredient.ingredient_id") // Directly from Link (Enums map to String
-                                                                          // auto)
+  @Mapping(target = "name", source = "ingredient.name")
+  @Mapping(target = "amount", source = "amount")
+  @Mapping(target = "unit", source = "unit")
+  @Mapping(target = "ingredient_id", source = "ingredient.ingredient_id")
+
   IngredientResponseDTO toIngredientDto(Recipe_Ingredient recipeIngredient);
 
   @Mapping(target = "ingredients", ignore = true)
